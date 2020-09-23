581b8c62dcb65d54327086bae13ba5b773db4524
Shemona Singh <singhshemona@gmail.com>
2020-09-22 21:11:20 -0400

    added range value and focus state

--- src/scripts/components/ui/Range/Range.tsx
+++ src/scripts/components/ui/Range/Range.tsx
@@ -1,6 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Range.scss';
 
 export const Range = () => {
-  return <input type="range" min="1" max="100" defaultValue="20" className="range" />;
+  const [value, isValue] = useState('20');
+
+  return (
+    <div>
+      <input
+        onChange={(event) => isValue(event.target.value)}
+        type="range"
+        min="1"
+        max="100"
+        defaultValue="20"
+        className="range"
+      />
+      <p className="value">{value}</p>
+    </div>
+  );
 };
