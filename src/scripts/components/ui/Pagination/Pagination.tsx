58eff562361f46fcb916a07431632c9a7973b37a
Shemona Singh <singhshemona@gmail.com>
2020-09-23 22:16:15 -0400

    set up pagination

---
+++ src/scripts/components/ui/Pagination/Pagination.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './Pagination.scss';
+
+export const Pagination = () => {
+  return (
+    <div>
+      <div className="slides">
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+      </div>
+      <div className="counter"></div>
+    </div>
+  );
+};
