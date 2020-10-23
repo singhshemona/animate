a29d7bdf5be55b631ea3fef037e1b8fa4a7c2419
Shemona Singh <singhshemona@gmail.com>
2020-10-23 14:43:44 -0400

    transferred over item selection styles

--- src/scripts/components/ui/CardEnlarge/CardEnlarge.tsx
+++ src/scripts/components/ui/CardEnlarge/CardEnlarge.tsx
@@ -27,25 +27,26 @@ export const CardEnlarge = () => {
       </div>
       <article>
         <p>
-          If we find life out there, and it's not us, we will deem it not intelligent. But what may be equally as likely
-          is that we find life that's vastly more intelligent than we are. If that's the case, we are putty in their
-          hands.
+          If we find life out there, and it&apos;s not us, we will deem it not intelligent. But what may be equally as
+          likely is that we find life that&apos;s vastly more intelligent than we are. If that&apos;s the case, we are
+          putty in their hands.
         </p>
         <p>
-          'Cosmos' is an occasion to bring everything that I have, all of my capacity to communicate. We may go to the
-          edge of the universe, but we're going to land right on you: in your heart, in your soul, in your mind.
+          &apos;Cosmos&apos; is an occasion to bring everything that I have, all of my capacity to communicate. We may
+          go to the edge of the universe, but we&apos;re going to land right on you: in your heart, in your soul, in
+          your mind.
         </p>
         <p>My goal is to have people know that they are participants in this great unfolding cosmic story.</p>
         <p>
           Once you have an innovation culture, even those who are not scientists or engineers - poets, actors,
           journalists - they, as communities, embrace the meaning of what it is to be scientifically literate. They
-          embrace the concept of an innovation culture. They vote in ways that promote it. They don't fight science and
-          they don't fight technology.
+          embrace the concept of an innovation culture. They vote in ways that promote it. They don&apos;t fight science
+          and they don&apos;t fight technology.
         </p>
         <p>
-          Let's say intelligence is your ability to compose poetry, symphonies, do art, math and science. Chimps can't
-          do any of that, yet we share 99 percent DNA. Everything that we are, that distinguishes us from chimps,
-          emerges from that one-percent difference.
+          Let&apos;s say intelligence is your ability to compose poetry, symphonies, do art, math and science. Chimps
+          can&apos;t do any of that, yet we share 99 percent DNA. Everything that we are, that distinguishes us from
+          chimps, emerges from that one-percent difference.
         </p>
       </article>
       <aside>
--- src/scripts/components/ui/ItemSelection/ItemSelection.tsx
+++ src/scripts/components/ui/ItemSelection/ItemSelection.tsx
@@ -5,15 +5,21 @@ export const ItemSelection = () => {
   const [isLift, setIsLifted] = useState(false);
 
   return (
-    <div className="card" onMouseEnter={() => setIsLifted(true)} onMouseLeave={() => setIsLifted(false)}>
+    <div className="card " onMouseEnter={() => setIsLifted(true)} onMouseLeave={() => setIsLifted(false)}>
       <div className={isLift ? 'lifted' : 'notLifted'}>
         <img src="https://via.placeholder.com/150" alt="placeholder" />
         <h2>Hello</h2>
         <p>
-          Exposing new ways to evolve our design language pulling teeth, nor let's schedule a standup during the sprint
-          to review our kpis and drop-dead date.
+          Exposing new ways to evolve our design language pulling teeth, nor let&apos;s schedule a standup during the
+          sprint to review our kpis and drop-dead date. It&apos;s not hard guys gain traction prepare yourself to swim
+          with the sharks drink from the firehose
         </p>
-        <button>We want this to appear on hover</button>
+        {isLift && (
+          <div className={'toggleContent'}>
+            <button className="select">We want this to appear on hover</button>
+            <p className="disclaimer">Disclaimer: clicking on this will deliver a flower to your door</p>
+          </div>
+        )}
       </div>
     </div>
   );
