29b975403dc4db01cd8ceb23d8b930fb39e6e62e
Shemona Singh <singhshemona@gmail.com>
2020-10-23 14:08:11 -0400

    accordion styles mostly complete

--- src/scripts/components/ui/Accordion/Accordion.tsx
+++ src/scripts/components/ui/Accordion/Accordion.tsx
@@ -19,7 +19,11 @@ export const Accordion = () => {
           First Title
         </button>
         <div className={'content ' + (whichAccordion === 'first' ? 'first-show' : 'hide')}>
-          <p>This is the content</p>
+          <p>
+            Part of what it is to be scientifically-literate, it&apos;s not simply, &apos;Do you know what DNA is? Or
+            what the Big Bang is? That&apos;s an aspect of science literacy. The biggest part of it is do you know how
+            to think about information that&apos;s presented in front of you.
+          </p>
         </div>
       </div>
       <div className="selection">
@@ -27,7 +31,11 @@ export const Accordion = () => {
           Second Title
         </button>
         <div className={'content ' + (whichAccordion === 'second' ? 'second-show' : 'hide')}>
-          <p>This is more content</p>
+          <p>
+            Even with all our technology and the inventions that make modern life so much easier than it once was, it
+            takes just one big natural disaster to wipe all that away and remind us that, here on Earth, we&apos;re
+            still at the mercy of nature.
+          </p>
         </div>
       </div>
       <div className="selection">
@@ -35,7 +43,11 @@ export const Accordion = () => {
           Third Title
         </button>
         <div className={'content ' + (whichAccordion === 'third' ? 'third-show' : 'hide')}>
-          <p>This is another content</p>
+          <p>
+            Keep in mind that if you take a tour through a hospital and look at every machine with on and off switch
+            that is brought into the service of diagnosing the human condition, that machine is based on principles of
+            physics discovered by a physicist in a machine designed by an engineer.
+          </p>
         </div>
       </div>
     </div>
